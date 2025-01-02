82186d19badc166c7bf04a7df9faf41b4d256b2c
Raid-the-XPRT <raltamimi0001@stu.kau.edu.sa>
2025-01-02 06:18:26 -0800

    modified alu control to include branch signals

--- CX204-LAB6/CX204-LAB6.srcs/sources_1/imports/new/alu_control.sv
+++ CX204-LAB6/CX204-LAB6.srcs/sources_1/imports/new/alu_control.sv
@@ -36,7 +36,13 @@ module alu_control(
             end                        
             1   :begin
                 case({func7,func3[2],func3[1],func3[0]})
-                4'b0000: alu_ctrl=4'b0000;                
+                4'b0000:    alu_ctrl=4'b1000;
+                4'b0001:    alu_ctrl=4'b1000;
+                4'b0100:    alu_ctrl=4'b0010;
+                4'b0101:    alu_ctrl=4'b0011;
+                4'b0110:    alu_ctrl=4'b0010;
+                4'b0111:    alu_ctrl=4'b0011;
+                default:    alu_ctrl=4'b0000;
                 endcase
             end            
             2   :begin
